1691bd6df2ea3f7156cb92b672ae0fe13605cdc0
Lowell Bander <lowellbander@gmail.com>
2014-11-27 23:38:03 -0800

    refactored for readibility

--- project2_204156534/client.c
+++ project2_204156534/client.c
@@ -13,27 +13,27 @@
 int main(int argc, char *argv[]) {
     struct sockaddr_in server;
     int len = sizeof(struct sockaddr_in);
-    char buf[BUF_SIZE];
+    char buffer[BUF_SIZE];
     struct hostent *host;
-    int n, s, port;
+    int message_length, sock, port;
 
     if (argc < 4) {
-    fprintf(stderr, "Usage: %s <host> <port> <message>\n", argv[0]);
-    return 1;
+        fprintf(stderr, "Usage: %s <host> <port> <message>\n", argv[0]);
+        return 1;
     }
 
     host = gethostbyname(argv[1]);
     if (host == NULL) {
-    perror("gethostbyname");
-    return 1;
+        perror("gethostbyname");
+        return 1;
     }
 
     port = atoi(argv[2]);
 
     /* initialize socket */
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-    perror("socket");
-    return 1;
+    if ((sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        perror("socket");
+        return 1;
     }
 
     /* initialize server addr */
@@ -43,9 +43,9 @@ int main(int argc, char *argv[]) {
     server.sin_addr = *((struct in_addr*) host->h_addr);
 
     /* send message */
-    if (sendto(s, argv[3], strlen(argv[3]), 0, (struct sockaddr *) &server, len) == -1) {
-    perror("sendto()");
-    return 1;
+    if (sendto(sock, argv[3], strlen(argv[3]), 0, (struct sockaddr *) &server, len) == -1) {
+        perror("sendto()");
+        return 1;
     }
 
     /* receive echo.
@@ -53,15 +53,13 @@ int main(int argc, char *argv[]) {
     ** to stay in listen mode and thus function as a "server" - allowing it to 
     ** receive message sent from any endpoint.
     */
-    while ((n = recvfrom(s, buf, BUF_SIZE, 0, (struct sockaddr *) &server, &len)) != -1) {
-    printf("Received from %s:%d: ", 
-        inet_ntoa(server.sin_addr), 
-        ntohs(server.sin_port)); 
-    fflush(stdout);
-    write(1, buf, n);
-    write(1, "\n", 1);
+    while ((message_length = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr *) &server, &len)) != -1) {
+        printf("Received from %s:%d: ",  inet_ntoa(server.sin_addr), ntohs(server.sin_port)); 
+        fflush(stdout);
+        write(1, buffer, message_length);
+        write(1, "\n", 1);
     }
 
-    close(s);
+    close(sock);
     return 0;
 }
