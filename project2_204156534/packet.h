c115700090a2b6d940cdf90822a1d517a0dbeb29
Lowell Bander <lowellbander@gmail.com>
2014-12-05 00:07:15 -0800

    added room for null terminator in payload

--- project2_204156534/packet.h
+++ project2_204156534/packet.h
@@ -1,12 +1,13 @@
 //packet struct
 
 #define PACKET_SIZE 1024
-#define PAYLOAD_SIZE (PACKET_SIZE - 2*sizeof(long))
+#define NULL_TERMINATOR 1
+#define PAYLOAD_SIZE (PACKET_SIZE - 2*sizeof(long) - NULL_TERMINATOR)
 
 typedef struct{
     unsigned long seqnum;
     unsigned long total_size; 
-    char payload[PAYLOAD_SIZE];
+    char payload[PAYLOAD_SIZE + NULL_TERMINATOR];
 } packet;
 
 void print_packet(packet* packet_pointer) {
@@ -18,7 +19,7 @@ void print_packet(packet* packet_pointer) {
     printf("printing packet information: \n");
     printf("Seq num: %lu\n", packet_pointer->seqnum);
     printf("Total size: %lu\n", packet_pointer->total_size);
-    printf("Payload: %s\n", packet_pointer->payload);            
+    //printf("Payload: %s\n", packet_pointer->payload);            
     printf("strlen(payload): %i\n", strlen(packet_pointer->payload));
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
 }
