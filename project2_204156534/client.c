93a86428adfd3b3bf5d84a58067281f1c524dc2a
Lowell Bander <lowellbander@gmail.com>
2014-12-04 18:36:35 -0800

    succesfully initializes buffer for received packets

--- project2_204156534/client.c
+++ project2_204156534/client.c
@@ -53,6 +53,7 @@ int main(int argc, char *argv[]) {
     }
     printf("requesting file: '%s'\n", filename);
 
+    // TODO: delete this deprecated code
     /* receive echo.
     ** for single message, "while" is not necessary. But it allows the client 
     ** to stay in listen mode and thus function as a "server" - allowing it to 
@@ -65,6 +66,10 @@ int main(int argc, char *argv[]) {
     //     write(1, "\n", 1);
     // }
 
+    // TODO: initialize linked list of packets
+
+    packet* received_packets = NULL;
+
     while (true) {
         // wait until there is a packet in the buffer
         if ((message_length = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr *) &server, &len)) != -1) {
@@ -74,6 +79,19 @@ int main(int argc, char *argv[]) {
             printf("Seq num: %lu\n", packet_pointer->seqnum);
             printf("Total size: %lu\n", packet_pointer->total_size);
             printf("Payload: %s\n", packet_pointer->payload);            
+            
+            // TODO: initialize received_packets if we have not yet done so.
+            if (received_packets == NULL) {
+                int nPackets = ((packet_pointer->total_size)/PAYLOAD_SIZE)*sizeof(packet);
+                received_packets = (packet*)calloc(nPackets, sizeof(packet));
+                if (received_packets != NULL)
+                    printf("succesfully initialized received_packets.\n");
+                else 
+                    printf("failed to intitialize received_packets.\n");
+            }
+
+            //TODO: add the received packet to received_packets if appropriate
+
             //if (packet->)
         }
     }
